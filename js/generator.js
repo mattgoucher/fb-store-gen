8c8ebc3bfabc54daec313e0caa078db4f5eb82f3
Matt Goucher <matt@mattgoucher.com>
2013-07-28 23:18:44 -0700

    JS Clean, closure

--- js/generator.js
+++ js/generator.js
@@ -1,170 +1,173 @@
-var App         = angular.module("storemaker", ["storemaker.controllers"]),
-    Controllers = angular.module("storemaker.controllers", []);
+(function() {
 
-App.config(["$routeProvider", function($routeProvider) {
+    var App = angular.module("storemaker", []);
 
-    // Home
-    $routeProvider.when("/", {
-        controller: "generatorCtrl"
-    });
+    App.config(["$routeProvider", function($routeProvider) {
 
-    // Default
-    $routeProvider.otherwise({
-        redirectTo: "/"
-    });
+        // Home
+        $routeProvider.when("/", {
+            controller: "generatorCtrl"
+        });
 
-}]);
+        // Default
+        $routeProvider.otherwise({
+            redirectTo: "/"
+        });
 
-
-Controllers.controller("generatorCtrl", [
-    "$scope",
-    function ($scope) {
-
-        var previewFrame    = document.getElementById("preview-frame"),
-            outputTemplate  = document.getElementById("before-html").innerHTML,
-            outputTextbox   = document.getElementById("output-area"),
-            createdProducts = 0;
+    }]);
 
 
-        // Current Scope
-        $scope.products      = {};
-        $scope.storeSettings = {};
-        $scope.isEditing     = false;
-        $scope.output        = "Add a product, and generate.";
-        $scope.alert         = "Welcome.";
+    App.controller("generatorCtrl", [
+        "$scope",
+        function ($scope) {
+
+            var previewFrame    = document.getElementById("preview-frame"),
+                outputTemplate  = document.getElementById("before-html").innerHTML,
+                outputTextbox   = document.getElementById("output-area"),
+                createdProducts = 0;
 
 
-        /**
-         * Reset form models
-         * @return {undefined}
-         */
-        function resetFormsToPristine() {
-            $scope.product       = {};
-            $scope.editedProduct = {};
-        }
+            // Current Scope
+            $scope.products      = {};
+            $scope.storeSettings = {};
+            $scope.isEditing     = false;
+            $scope.output        = "Add a product, and generate.";
+            $scope.alert         = "Welcome.";
 
 
-        /**
-         * Get a product based on its id
-         * @param  {int} id product id
-         * @return {object} the product
-         */
-        function getProductById(id) {
-            return $scope.products[id];
-        }
-
-
-        function setAlert(message) {
-            $scope.alert = message;
-        }
-
-
-        /**
-         * Push product into products array
-         * @return {undefined}
-         */
-        $scope.addProduct = function() {
-
-            var product = $scope.product;
-
-            // Increment total count
-            createdProducts = createdProducts + 1;
-
-            // Add product Id to new product.
-            product.id = createdProducts;
-
-            // Push Product to array
-            $scope.products[createdProducts] = product;
-
-            // Reset forms
-            resetFormsToPristine();
-
-            // Alert user of action
-            setAlert("Product added successfully.");
-
-        };
-
-
-        /**
-         * Copy a product for editing
-         * @param  {object} product The product we are editing
-         * @return {undefined}
-         */
-        $scope.editProduct = function(product) {
-
-            // We're Editing
-            $scope.isEditing = true;
-
-            // Create a copy of an existing product
-            // so we don't overwrite the current settings first.
-            $scope.editedProduct = angular.copy(product);
-
-        };
-
-
-        /**
-         * Save changes to an existing product
-         * @return {undefined}
-         */
-        $scope.saveExistingProduct = function() {
-
-            var edits = $scope.editedProduct;
-
-            // Save over the current product
-            $scope.products[edits["id"]] = edits;
-
-            // Update alert
-            setAlert("Product " + edits.name + " saved.");
-
-            // No longer editing
-            $scope.isEditing = false;
-
-            // Reset forms
-            resetFormsToPristine();
-
-        };
-
-
-        /**
-         * Delete a product from memory
-         * @param  {object} product product to be deleted
-         * @return {undefine
-         */
-        $scope.deleteProduct = function(product) {
-
-            if (confirm("Delete. Seriously, Gone Forever")) {
-
-                // Note
-                setAlert(product.name + " deleted successfully.");
-
-                // DELETE KEY
-                delete $scope.products[product["id"]];
-
-            }else{
-                $scope.isEditing = false;
+            /**
+             * Reset form models
+             * @return {undefined}
+             */
+            function resetFormsToPristine() {
+                $scope.product       = {};
+                $scope.editedProduct = {};
             }
 
-        };
+
+            /**
+             * Get a product based on its id
+             * @param  {int} id product id
+             * @return {object} the product
+             */
+            function getProductById(id) {
+                return $scope.products[id];
+            }
 
 
-        /**
-         * Parse HTML templates, spit it out
-         * @return {string} HTML markup for the store
-         */
-        $scope.generate = function() {
+            function setAlert(message) {
+                $scope.alert = message;
+            }
 
-            var finalSettings    = { store: $scope.storeSettings, products: $scope.products },
-                compiledTemplate = _.template(outputTemplate, finalSettings);
 
-            // set iFrame HTML
-            previewFrame.contentDocument.documentElement.innerHTML = compiledTemplate;
+            /**
+             * Push product into products array
+             * @return {undefined}
+             */
+            $scope.addProduct = function() {
 
-            // Set textarea 
-            outputTextbox.value = compiledTemplate;
+                var product = $scope.product;
 
-            setAlert("Successfully generated");
+                // Increment total count
+                createdProducts = createdProducts + 1;
+
+                // Add product Id to new product.
+                product.id = createdProducts;
+
+                // Push Product to array
+                $scope.products[createdProducts] = product;
+
+                // Reset forms
+                resetFormsToPristine();
+
+                // Alert user of action
+                setAlert("Product added successfully.");
+
+            };
+
+
+            /**
+             * Copy a product for editing
+             * @param  {object} product The product we are editing
+             * @return {undefined}
+             */
+            $scope.editProduct = function(product) {
+
+                // We're Editing
+                $scope.isEditing = true;
+
+                // Create a copy of an existing product
+                // so we don't overwrite the current settings first.
+                $scope.editedProduct = angular.copy(product);
+
+            };
+
+
+            /**
+             * Save changes to an existing product
+             * @return {undefined}
+             */
+            $scope.saveExistingProduct = function() {
+
+                var edits = $scope.editedProduct;
+
+                // Save over the current product
+                $scope.products[edits["id"]] = edits;
+
+                // Update alert
+                setAlert("Product " + edits.name + " saved.");
+
+                // No longer editing
+                $scope.isEditing = false;
+
+                // Reset forms
+                resetFormsToPristine();
+
+            };
+
+
+            /**
+             * Delete a product from memory
+             * @param  {object} product product to be deleted
+             * @return {undefine
+             */
+            $scope.deleteProduct = function(product) {
+
+                if (confirm("Delete. Seriously, Gone Forever")) {
+
+                    // Note
+                    setAlert(product.name + " deleted successfully.");
+
+                    // DELETE KEY
+                    delete $scope.products[product["id"]];
+
+                }else{
+                    $scope.isEditing = false;
+                }
+
+            };
+
+
+            /**
+             * Parse HTML templates, spit it out
+             * @return {string} HTML markup for the store
+             */
+            $scope.generate = function() {
+
+                var finalSettings    = { store: $scope.storeSettings, products: $scope.products },
+                    compiledTemplate = _.template(outputTemplate, finalSettings);
+
+                // set iFrame HTML
+                previewFrame.contentDocument.documentElement.innerHTML = compiledTemplate;
+
+                // Set textarea 
+                outputTextbox.value = compiledTemplate;
+
+                setAlert("Successfully generated");
+
+            }
 
         }
+    ]);
 
-    }
-]);
+}());
